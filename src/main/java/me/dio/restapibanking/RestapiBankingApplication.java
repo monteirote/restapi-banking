7e1f968ca3b5096be1aec43b20cdfee5b8cafa3b
Vinicius <vinicius@iconax.com.br>
2023-10-09 19:03:54 -0300

    configurações iniciais

---
+++ src/main/java/me/dio/restapibanking/RestapiBankingApplication.java
@@ -0,0 +1,12 @@
+package me.dio.restapibanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiBankingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/restapibanking/RestapiBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.restapibanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
